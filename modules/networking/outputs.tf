9de89f6fbd4427e38a7d236d9ca036031a3baec3
william-darius-winner <william.darius.winner@pm.me>
2026-03-22 12:02:20 -0500

    Create outputs.tf

---
+++ modules/networking/outputs.tf
@@ -0,0 +1,24 @@
+output "resource_group_name" {
+  description = "Name of the created resource group"
+  value       = azurerm_resource_group.main.name
+}
+
+output "resource_group_id" {
+  description = "ID of the created resource group"
+  value       = azurerm_resource_group.main.id
+}
+
+output "vnet_id" {
+  description = "ID of the virtual network"
+  value       = azurerm_virtual_network.main.id
+}
+
+output "aks_subnet_id" {
+  description = "ID of the AKS subnet"
+  value       = azurerm_subnet.aks.id
+}
+
+output "nsg_id" {
+  description = "ID of the AKS network security group"
+  value       = azurerm_network_security_group.aks.id
+}
