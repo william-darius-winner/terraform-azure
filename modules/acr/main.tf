94792fc283c062dc42815192963b569af2ebee19
william-darius-winner <william.darius.winner@pm.me>
2026-03-29 21:32:35 -0500

    Update main.tf - resolving terraform plan errors

--- modules/acr/main.tf
+++ modules/acr/main.tf
@@ -1,1 +1,16 @@
+resource "azurerm_container_registry" "main" {
+  name                = var.registry_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = var.sku
+  admin_enabled       = false
 
+  tags = var.tags
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                            = azurerm_container_registry.main.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = var.aks_kubelet_identity_id
+  skip_service_principal_aad_check = true
+}
